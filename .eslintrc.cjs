1b31b3bf6132d981d0ff931863b381652e3dafd4
jiumingmao <879021430@qq.com>
2023-10-15 21:40:26 +0800

    chore: 🔧 eslint增加node运行环境

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,12 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  // 运行环境
+  env: {
+    node: true,
+    es6: true
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
