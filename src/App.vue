354be542bd490c8b6cc17cf688fa89589e328b29
jiumingmao <879021430@qq.com>
2023-10-16 20:39:31 +0800

    test: ✅ tailwindcss初体验

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <script setup></script>
 
 <template>
-  <div></div>
+  <div class="bg-slate-800 rounded-xl p-8 md:flex md:p-0">
+    <img
+      src="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&h=1280&q=80"
+      alt=""
+      class="w-24 h-24 rounded-full mx-auto md:w-48 md:h-auto md:rounded-none"
+    />
+
+    <div class="pt-6 text-center space-y-4 md:p-8 md:text-left">
+      <p class="text-lg font-medium text-zinc-100">
+        “这是使用 tailwindcss
+        来实现的一个基础的卡片区域，在实现该卡片区域的过程中，我们从来没有离开过 HTML 区域.”
+      </p>
+      <div class="font-medium">
+        <div class="text-sky-400">LGD_Sunday</div>
+        <div class="text-slate-500">中前台前端解决方案</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
