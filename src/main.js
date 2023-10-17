42bfd84ad2ecd2382a8bcd377b95c8b0e39c7f52
jiumingmao <879021430@qq.com>
2023-10-17 22:08:32 +0800

    feat: ✨ 设置rem 基准并修正 tailwind 字体rem大小

--- src/main.js
+++ src/main.js
@@ -7,4 +7,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { useREM } from './utils/flexible.js'
+useREM()
+
 createApp(App).use(createPinia()).use(router).mount('#app')
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      fontSize: {
+        xs: ['0.25rem', '0.35rem'],
+        sm: ['0.35rem', '0.45rem'],
+        base: ['0.42rem', '0.52rem'],
+        lg: ['0.55rem', '0.65rem'],
+        xl: ['0.65rem', '0.75rem']
+      }
+    }
   },
   plugins: []
 }
