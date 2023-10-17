25e899581599903819f265e46af61716346517dd
jiumingmao <879021430@qq.com>
2023-10-17 20:50:26 +0800

    feat: ✨ axios配置响应拦截器简化数据获取流程

--- src/utils/request.js
+++ src/utils/request.js
@@ -13,4 +13,16 @@ service.interceptors.request.use((config) => {
   return config
 })
 
+// 响应拦截器
+service.interceptors.response.use((response) => {
+  const { success, message, data } = response.data
+  //   要根据success的成功与否决定下面的操作
+  if (success) {
+    return data
+  } else {
+    // TODO：业务错误
+    return Promise.reject(new Error(message))
+  }
+})
+
 export default service
