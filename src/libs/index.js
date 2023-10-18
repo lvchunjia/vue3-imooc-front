415fae8691d9b4b5271f0b1d6004fa9b270c5f46
jiumingmao <879021430@qq.com>
2023-10-18 22:16:36 +0800

    feat: ✨ 实现lib物料库自动导入功能

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,9 +1,15 @@
-import svgIcon from './svg-icon/index.vue'
-import popup from './popup/index.vue'
+import { defineAsyncComponent } from 'vue'
 
 export default {
   install(app) {
-    app.component('m-svg-icon', svgIcon)
-    app.component('m-popup', popup)
+    // 获取当前路径任意文件夹下的 index.vue 文件
+    const components = import.meta.glob('./*/index.vue')
+    // 遍历获取到的组件模块
+    for (const [fullPath, fn] of Object.entries(components)) {
+      // 拼接组件注册的 name
+      const componentName = 'm-' + fullPath.replace('./', '').split('/')[0]
+      // 通过 defineAsyncComponent 异步导入指定路径下的组件
+      app.component(componentName, defineAsyncComponent(fn))
+    }
   }
 }
