91855f8e4cb9eb7f42c60d7a0fab5d4151b04eae
jiumingmao <879021430@qq.com>
2023-10-18 21:12:27 +0800

    feat: ✨ 移动端nav-bar横向导航完成

--- src/views/main/components/navigation/index.vue
+++ src/views/main/components/navigation/index.vue
@@ -1,12 +1,24 @@
 <script setup>
+import { ref } from 'vue'
 import { isMobile } from '@/utils/flexible.js'
 import mobileNav from './mobile/index.vue'
 import pcNav from './pc/index.vue'
+
+import { getCategory } from '@/api/category'
+import { ALL_CATEGORY_ITEM } from '@/constants'
+
+const categoryData = ref([])
+const getCategoryData = async () => {
+  const { categorys } = await getCategory()
+  categoryData.value = categorys
+  categoryData.value.unshift(ALL_CATEGORY_ITEM)
+}
+getCategoryData()
 </script>
 
 <template>
   <div class="navigation">
-    <mobile-nav v-if="isMobile" />
+    <mobile-nav v-if="isMobile" :data="categoryData" />
     <pc-nav v-else />
   </div>
 </template>
--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,16 +1,94 @@
 <script setup>
-import { getCategory } from '@/api/category'
+import { useScroll } from '@vueuse/core'
+import { onBeforeUpdate, ref, watch } from 'vue'
 
-const getCategoryData = async () => {
-  const res = await getCategory()
-  console.log(res)
+defineProps({
+  data: {
+    type: Array,
+    required: true
+  }
+})
+
+// 滑块位置
+const sliderStyle = ref({
+  transform: 'translateX(0px)',
+  width: '52px'
+})
+// 选中的 item 下标
+const currentCategoryIndex = ref(0)
+
+/**
+ * 获取填充的所有 item 元素
+ */
+let itemRefs = []
+// 每次dom更新都会执行
+const setItemRef = (el) => {
+  if (el) {
+    itemRefs.push(el)
+  }
 }
-getCategoryData()
+onBeforeUpdate(() => {
+  itemRefs = []
+})
+
+/**
+ * 计算偏移位置
+ */
+// 获取ul元素
+const ulTarget = ref(null)
+const { x: ulScrollLeft } = useScroll(ulTarget)
+watch(currentCategoryIndex, (val) => {
+  // 获取选中元素的 left、width
+  const { left, width } = itemRefs[val].getBoundingClientRect()
+  // 为 sliderStyle 设置属性
+  sliderStyle.value = {
+    // ul 横向滚动位置 + 当前元素的 left 偏移量 - ul-padding
+    transform: `translateX(${ulScrollLeft.value + left - 10 + 'px'})`,
+    width: width + 'px'
+  }
+})
+
+/**
+ * item 点击事件
+ * @param {*} index
+ */
+const onItemClick = (index) => {
+  currentCategoryIndex.value = index
+}
 </script>
 
 <template>
-  <div class="mobile">
-    <h2>mobile</h2>
+  <div class="bg-white sticky top-0 left-0 z-1">
+    <ul
+      ref="ulTarget"
+      class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden"
+    >
+      <!-- 汉堡按钮 -->
+      <li class="z-20 fixed top-0 right-[-1px] h-4 px-1 flex items-center bg-white shadow-l-white">
+        <m-svg-icon class="w-1.5 h-1.5" name="hamburger"></m-svg-icon>
+      </li>
+
+      <!-- 滑块 -->
+      <li
+        ref="sliderTarget"
+        class="absolute h-[22px] bg-zinc-900 rounded-lg duration-200"
+        :style="sliderStyle"
+      ></li>
+
+      <!-- category item -->
+      <li
+        v-for="(item, index) in data"
+        :key="item.id"
+        :ref="setItemRef"
+        @click="onItemClick(index)"
+        :class="{
+          'text-zinc-100 ': currentCategoryIndex === index
+        }"
+        class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
+      >
+        {{ item.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
