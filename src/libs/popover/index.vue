9b046ceba427254c0ca9401c2e0b21d8595f8906
jiumingmao <879021430@qq.com>
2023-10-19 21:40:30 +0800

    feat(PC端): ✨ 完善popover

--- src/libs/popover/index.vue
+++ src/libs/popover/index.vue
@@ -1,34 +1,126 @@
 <script setup>
-import { ref } from 'vue'
+import { nextTick, ref, watch } from 'vue'
+import {
+  placementEnum,
+  PROP_TOP_LEFT,
+  PROP_TOP_RIGHT,
+  PROP_BOTTOM_LEFT,
+  PROP_BOTTOM_RIGHT
+} from './constants'
+
+const props = defineProps({
+  // 控制气泡弹出位置，并给出开发者错误的提示
+  placement: {
+    type: String,
+    default: 'bottom-left',
+    validator(val) {
+      const result = placementEnum.includes(val)
+      if (!result) {
+        throw new Error(`你的 placement 必须是 ${placementEnum.join('、')} 中的一个`)
+      }
+      return result
+    }
+  }
+})
 
 // 控制 menu 展示
 const isVisable = ref(false)
+// 延迟关闭时长
+const DELAY_TIME = 100
+// 控制延迟关闭
+let timeout = null
 
 /**
  * 鼠标移入的触发行为
  */
 const onMouseenter = () => {
   isVisable.value = true
+  // 再次触发时，清理延时装置
+  if (timeout) {
+    clearTimeout(timeout)
+  }
 }
 
 /**
  * 鼠标移出的触发行为
  */
 const onMouseleave = () => {
-  isVisable.value = false
+  // 延时装置
+  timeout = setTimeout(() => {
+    isVisable.value = false
+    timeout = null
+  }, DELAY_TIME)
 }
+
+/**
+ * 计算元素尺寸
+ */
+const referenceTarget = ref(null)
+const contentTarget = ref(null)
+const useElementSize = (target) => {
+  if (!target) return {}
+  return {
+    width: target.offsetWidth,
+    height: target.offsetHeight
+  }
+}
+
+/**
+ * 计算弹层位置
+ */
+const contentStyle = ref({
+  top: 0,
+  left: 0
+})
+
+/**
+ * 监听展示的变化，在展示时计算气泡位置
+ */
+watch(isVisable, (val) => {
+  if (!val) return
+  // 等待渲染成功之后
+  nextTick(() => {
+    switch (props.placement) {
+      // 左上
+      case PROP_TOP_LEFT:
+        contentStyle.value.top = 0
+        contentStyle.value.left = -useElementSize(contentTarget.value).width + 'px'
+        break
+      // 右上
+      case PROP_TOP_RIGHT:
+        contentStyle.value.top = 0
+        contentStyle.value.left = useElementSize(referenceTarget.value).width + 'px'
+        break
+      // 左下
+      case PROP_BOTTOM_LEFT:
+        contentStyle.value.top = useElementSize(referenceTarget.value).height + 'px'
+        contentStyle.value.left = -useElementSize(contentTarget.value).width + 'px'
+        break
+      // 右下
+      case PROP_BOTTOM_RIGHT:
+        contentStyle.value.top = useElementSize(referenceTarget.value).height + 'px'
+        contentStyle.value.left = useElementSize(referenceTarget.value).width + 'px'
+        break
+    }
+  })
+})
 </script>
 
 <template>
   <div class="relative" @mouseleave="onMouseleave" @mouseenter="onMouseenter">
-    <div>
+    <div ref="referenceTarget">
       <!-- 具名插槽 -->
       <slot name="reference" />
     </div>
 
     <!-- 气泡展示动画 -->
     <transition name="slide">
-      <div v-show="isVisable" class="absolute p-1 z-20 bg-white border rounded-md">
+      <div
+        ref="contentTarget"
+        v-show="isVisable"
+        :style="contentStyle"
+        class="absolute p-1 z-20 bg-white border rounded-md"
+      >
         <!-- 匿名插槽 -->
         <slot />
       </div>
--- src/views/layout/components/header/header-theme.vue
+++ src/views/layout/components/header/header-theme.vue
@@ -1,9 +1,56 @@
-<script setup></script>
+<script setup>
+import { THEME_DARK, THEME_LIGHT, THEME_SYSTEM } from '@/constants'
+
+// 构建渲染数据源
+const themeArr = [
+  {
+    id: '0',
+    type: THEME_LIGHT,
+    icon: 'theme-light',
+    name: '极简白'
+  },
+  {
+    id: '1',
+    type: THEME_DARK,
+    icon: 'theme-dark',
+    name: '极夜黑'
+  },
+  {
+    id: '2',
+    type: THEME_SYSTEM,
+    icon: 'theme-system',
+    name: '跟随系统'
+  }
+]
+</script>
 
 <template>
-  <div class="header-theme">
-    <h2>theme</h2>
-  </div>
+  <m-popover>
+    <!-- 具名插槽 -->
+    <template #reference>
+      <m-svg-icon
+        name="theme-light"
+        class="guide-theme w-4 h-4 p-1 cursor-pointer rounded-sm duration-200 outline-none hover:bg-zinc-100/60"
+        fillClass="fill-zinc-900"
+      ></m-svg-icon>
+    </template>
+
+    <!-- 匿名插槽 -->
+    <div class="w-[140px] overflow-hidden">
+      <div
+        class="flex items-center p-1 cursor-pointer rounded hover:bg-zinc-100/60"
+        v-for="item in themeArr"
+        :key="item.id"
+      >
+        <m-svg-icon
+          :name="item.icon"
+          class="w-1.5 h-1.5 mr-1"
+          fillClass="fill-zinc-900"
+        ></m-svg-icon>
+        <span class="text-zinc-800 text-sm">{{ item.name }}</span>
+      </div>
+    </div>
+  </m-popover>
 </template>
 
 <style scoped lang="scss"></style>
