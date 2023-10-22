3a7b22c1abce6b995eb5b78cfcb0910c03a357ea
jiumingmao <879021430@qq.com>
2023-10-22 18:45:26 +0800

    feat: ✨ token 超时处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 import { storeToRefs } from 'pinia'
 import { useUserStore } from '@/store/modules/user'
 
+const userStore = useUserStore()
+
 const service = axios.create({
   baseURL: import.meta.env.VITE_BASE_API,
   timeout: 5000
@@ -11,7 +13,7 @@ const service = axios.create({
 service.interceptors.request.use((config) => {
   // 添加 icode
   config.headers.icode = '821EEDAC4EA3EDC5'
-  const userStore = useUserStore()
+
   const { token } = storeToRefs(userStore)
   if (token.value) {
     // 如果token存在 注入token
@@ -22,15 +24,27 @@ service.interceptors.request.use((config) => {
 })
 
 // 响应拦截器
-service.interceptors.response.use((response) => {
-  const { success, message, data } = response.data
-  //   要根据success的成功与否决定下面的操作
-  if (success) {
-    return data
-  } else {
-    // TODO：业务错误
-    return Promise.reject(new Error(message))
+service.interceptors.response.use(
+  (response) => {
+    const { success, message, data } = response.data
+    //   要根据success的成功与否决定下面的操作
+    if (success) {
+      return data
+    } else {
+      // TODO：业务错误
+      return Promise.reject(new Error(message))
+    }
+  },
+  (error) => {
+    // 处理 token 超时问题
+    if (error.response && error.response.data && error.response.data.code === 401) {
+      const { logout } = userStore
+      // TODO: token超时
+      logout()
+    }
+    // TODO: 提示错误消息
+    return Promise.reject(error)
   }
-})
+)
 
 export default service
