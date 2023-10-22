33ab37720871e318910eed5eebc656339b5ec2e6
jiumingmao <879021430@qq.com>
2023-10-22 17:30:34 +0800

    feat: ✨ 通过SliderCaptcha实现login登陆人类行为验证

--- src/views/login-register/login/index.vue
+++ src/views/login-register/login/index.vue
@@ -1,13 +1,27 @@
 <script setup>
+import { ref } from 'vue'
 import { Form as VeeForm, Field as VeeField, ErrorMessage as VeeErrorMessage } from 'vee-validate'
 import LoginHeader from '../components/header.vue'
+import sliderCaptchaVue from './slider-captcha.vue'
 import { validateUsername, validatePassword } from '../validate'
 
 /**
  * 登录触发
  */
 const onLoginHandler = () => {
-  console.log('触发登录')
+  isSliderCaptchaVisible.value = true
+}
+
+// 控制 sliderCaptcha 展示
+const isSliderCaptchaVisible = ref(false)
+
+/**
+ * 人类行为验证通过
+ */
+const onCaptchaSuccess = async () => {
+  isSliderCaptchaVisible.value = false
+  // 登录操作
+  console.log('执行登录操作')
 }
 </script>
 
@@ -34,8 +48,7 @@ const onLoginHandler = () => {
           autocomplete="on"
           class="dark:bg-zinc-800 dark:text-zinc-400 border-b-zinc-400 border-b-[1px] w-full outline-0 pb-1 px-1 text-base focus:border-b-main dark:focus:border-b-zinc-200 xl:dark:bg-zinc-900"
         />
-        <vee-error-message class="text-sm text-red-600 block mt-0.5 text-left" name="username">
-        </vee-error-message>
+        <vee-error-message name="username" class="text-sm text-red-600 block mt-0.5 text-left" />
 
         <vee-field
           :rules="validatePassword"
@@ -45,8 +58,7 @@ const onLoginHandler = () => {
           autocomplete="on"
           class="dark:bg-zinc-800 dark:text-zinc-400 border-b-zinc-400 border-b-[1px] w-full outline-0 pb-1 px-1 text-base focus:border-b-main dark:focus:border-b-zinc-200 xl:dark:bg-zinc-900"
         />
-        <vee-error-message class="text-sm text-red-600 block mt-0.5 text-left" name="password">
-        </vee-error-message>
+        <vee-error-message name="password" class="text-sm text-red-600 block mt-0.5 text-left" />
 
         <div class="pt-1 pb-3 leading-[0px] text-right">
           <a
@@ -68,6 +80,13 @@ const onLoginHandler = () => {
         <m-svg-icon class="w-4 cursor-pointer" name="wexin"></m-svg-icon>
       </div>
     </div>
+
+    <!-- 人类行为验证模块 -->
+    <slider-captcha-vue
+      v-if="isSliderCaptchaVisible"
+      @close="isSliderCaptchaVisible = false"
+      @success="onCaptchaSuccess"
+    />
   </div>
 </template>
 
