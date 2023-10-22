d9ce0aa3c618cb36ba1cc84cef5344584bb28215
jiumingmao <879021430@qq.com>
2023-10-22 16:37:44 +0800

    feat: ✨ 详情页实现

--- src/views/pins/index.vue
+++ src/views/pins/index.vue
@@ -1,8 +1,10 @@
-<script setup></script>
+<script setup>
+import PinsVue from './components/pins.vue'
+</script>
 
 <template>
-  <div class="home">
-    <h2>home</h2>
+  <div class="w-full h-full bg-zinc-200 dark:bg-zinc-800">
+    <pins-vue :id="$route.params.id" />
   </div>
 </template>
 
