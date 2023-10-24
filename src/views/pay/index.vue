ab46a4a04878fab0a797f04338fe2c4c811bcc18
jiumingmao <879021430@qq.com>
2023-10-24 23:35:42 +0800

    no message

--- src/views/pay/index.vue
+++ src/views/pay/index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
-import useUserStore from '@/store/user'
+import { useUserStore } from '@/store/modules/user'
 import { getPayResult } from '@/api/pay'
 
 const route = useRoute()
